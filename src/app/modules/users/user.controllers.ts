1225c5d3c76a028abdecb80445d72ab557e0bced
Tausif Ahmed <tausif.ritu1@gmail.com>
2023-11-24 09:47:49 +0600

    I have learned new about pass filed not inclueded in the response

--- src/app/modules/users/user.controllers.ts
+++ src/app/modules/users/user.controllers.ts
@@ -5,11 +5,11 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const { user: userData } = req.body;
     const result = await UserServices.createUserIntoDB(userData);
-
+    const { password, ...withoutPass } = result.toObject(); //Newly created user object. Make sure that the password field is not included in the response data.
     res.status(200).json({
       success: true,
       massage: 'student created succesfully',
-      data: result,
+      data: withoutPass,
     });
   } catch (error) {
     res.status(500).json({
